f560643f54fd191c32091b0790857416e0b89ca6
Juan David Torres Vasquez <jdtorres31@gmail.com>
2020-11-26 11:40:48 -0500

    Cambios de servicio de los jueves
    Cambios para inicio de los servicios de jueves

--- ControlCovid/ControlCovid/Controllers/HomeController.cs
+++ ControlCovid/ControlCovid/Controllers/HomeController.cs
@@ -12,6 +12,9 @@ namespace ControlCovid.Controllers
         public ActionResult Index()
         {
             Asistente datos = new Asistente();
+            ViewBag.MostrarFormulario = false;
+            ViewBag.Diagnostico = true;
+            ViewBag.MostrarModal = false;
             return View("Index",datos);
         }
 
@@ -31,12 +34,18 @@ namespace ControlCovid.Controllers
                     ViewBag.Mensaje = $"Registro exitoso, su codigo de registro es:  {data.Mensaje}.";
                     prmAsistente = new Asistente();
                     ModelState.Clear();
+                    ViewBag.MostrarModal = false;
+                    ViewBag.MostrarFormulario = true;
+                    ViewBag.Diagnostico = false;                    
+                    Response.AddHeader("REFRESH", "7;URL=/Home/Index");
                     return View("Index",prmAsistente);
                 }
                 else
                 {
 
                     ViewBag.TheResult = false;
+                    ViewBag.MostrarModal = false;
+                    ViewBag.MostrarFormulario = true;
                     ViewBag.Mensaje = $"No se pudo registrar su solicitud por el siguiente motivo: {data.Mensaje}";
                     return View("Index", prmAsistente);
                 }
@@ -45,7 +54,8 @@ namespace ControlCovid.Controllers
             }
             else
             {
-              
+                ViewBag.MostrarFormulario = true;
+                ViewBag.MostrarModal = false;
                 return View("Index", prmAsistente);
             }
 
@@ -54,7 +64,48 @@ namespace ControlCovid.Controllers
         public ActionResult GuardarAsistente()
         {
             Asistente prmAsistente = new Asistente();
+            ViewBag.MostrarFormulario = false;
+            ViewBag.MostrarModal = false;
+            ViewBag.Diagnostico = true;
             return View("Index", prmAsistente);
+        }
+
+        public ActionResult GuardarDiagnostico()
+        {
+            Asistente prmAsistente = new Asistente();
+            ViewBag.MostrarFormulario = false;
+            ViewBag.MostrarModal = false;
+            ViewBag.Diagnostico = true;
+            return View("Index", prmAsistente);
+        }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult GuardarDiagnostico(FormCollection prmFormulario)
+        {
+            //(Convert.ToBoolean(prmFormulario[""]))
+            if (((Convert.ToBoolean(prmFormulario["Fiebre"].Split(',')[0]) || (Convert.ToBoolean(prmFormulario["Respirar"].Split(',')[0]))|| (Convert.ToBoolean(prmFormulario["Garganta"].Split(',')[0]))
+                || (Convert.ToBoolean(prmFormulario["Olfato"].Split(',')[0])) || (Convert.ToBoolean(prmFormulario["Olfato"].Split(',')[0])) || (Convert.ToBoolean(prmFormulario["Tos"].Split(',')[0]))
+                || (Convert.ToBoolean(prmFormulario["Fatiga"].Split(',')[0])))==false) && (Convert.ToBoolean(prmFormulario["Respuesta"])==false))
+            {
+                ViewBag.MostrarFormulario = true;
+                ViewBag.Diagnostico = false;
+                ModelState.Clear();
+                Asistente prmAsistente = new Asistente();                
+                return View("Index", prmAsistente);
+            }
+            else
+            {
+                prmFormulario.Clear();
+                Asistente datos = new Asistente();
+                ViewBag.MostrarFormulario = false;
+                ViewBag.Diagnostico = true;
+                ViewBag.MostrarModal = true;
+                return View("Index", datos);
+            }
+          
         }
 
     }
---
+++ ControlCovid/ControlCovid/Models/Preguntas.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ControlCovid.Models
+{
+    public class Preguntas
+    {
+        public string Respuesta { get; set; }
+
+    }
+}
